d871b81d584a8f9846806f2a8221de229f14835d
lm317379829 <lm317379829@163.com>
2024-07-20 19:20:17 +0800

    Add files via upload

--- main.go
+++ main.go
@@ -189,7 +189,7 @@ func uploadIP(ip, name string, domain string, email string, key string) {
 		json.Unmarshal(body, &dnsRecords)
 		resultList := dnsRecords["result"].([]interface{})
 		rid := ""
-		proxiable := false
+		proxied := false
 		for _, record := range resultList {
 			if record.(map[string]interface{})["type"].(string) == "A" {
 				rid = record.(map[string]interface{})["id"].(string)
@@ -203,7 +203,7 @@ func uploadIP(ip, name string, domain string, email string, key string) {
 			"type":      "A",
 			"name":      fmt.Sprintf("%s.%s", name, domain),
 			"content":   ip,
-			"proxied": proxied,
+			"proxied":   proxied,
 		}
 		if rid == "" {
 			continue
